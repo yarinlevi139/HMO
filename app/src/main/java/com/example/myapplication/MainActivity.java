1b52847b1b5baec13428a9c1fd269a898f2d5864
Yuval <yuvalyurzd@gmail.com>
2024-01-03 17:53:44 +0200

    update

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -11,6 +13,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        System.out.println("ODIE");
+        Button login = findViewById(R.id.login_button);
+        login.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                //Toast.makeText(MainActivity.this,"nigger",Toast.LENGTH_SHORT).show();
+            }
+        });
+        Button sign_up = findViewById(R.id.signup_button);
+        sign_up.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                //Toast.makeText(MainActivity.this,"nigger",Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
