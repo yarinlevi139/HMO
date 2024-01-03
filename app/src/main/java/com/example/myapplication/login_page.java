ffb00c8573d2279167811ddd481d5ce034240758
Yuval <yuvalyurzd@gmail.com>
2024-01-03 20:47:10 +0200

    update

--- app/src/main/java/com/example/myapplication/login_page.java
+++ app/src/main/java/com/example/myapplication/login_page.java
@@ -1,25 +1,70 @@
 package com.example.myapplication;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class login_page extends AppCompatActivity {
+    private FirebaseAuth mAuth;
+
+
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login);
-        Button back_lgn = findViewById(R.id.backbtnlg);
-        back_lgn.setOnClickListener(new View.OnClickListener() {
+
+        mAuth = FirebaseAuth.getInstance();
+        EditText lg_box = findViewById(R.id.email);
+        EditText pass_box = findViewById(R.id.password);
+
+        Button sign_up_btn = findViewById(R.id.login);
+        sign_up_btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent n = new Intent(login_page.this,MainActivity.class);
-                startActivity(n);
+                String email = lg_box.getText().toString();
+                String password = pass_box.getText().toString();
+                signIn(email, password);
             }
         });
     }
 
+    private void signIn(String email, String password) {
+        if (TextUtils.isEmpty(email) || TextUtils.isEmpty(password)) {
+            Toast.makeText(login_page.this, "Please enter email and password", Toast.LENGTH_SHORT).show();
+            return;
+        }
 
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(login_page.this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            Toast.makeText(login_page.this, "Authentication successful.", Toast.LENGTH_SHORT).show();
+
+                            // Start another activity or perform further actions
+                            Intent intent = new Intent(login_page.this, main_page_member.class);
+                            startActivity(intent);
+                            finish(); // Finish LoginActivity so the user cannot go back to it using the back button
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Toast.makeText(login_page.this, "Authentication failed.", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+    }
 }
---
+++ app/src/main/java/com/example/myapplication/main_page_member.java
@@ -0,0 +1,13 @@
+package com.example.myapplication;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+public class main_page_member extends AppCompatActivity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_member);
+    }
+}
