39053434e2421e192bb6c05ab42cd20554cc4225
yarin <yarinlevi139@gmail.com>
2024-01-03 18:51:58 +0200

    sign up

--- app/src/main/java/com/example/myapplication/login_page.java
+++ app/src/main/java/com/example/myapplication/login_page.java
@@ -2,12 +2,23 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class login_page extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login);
+        Button back_lgn = findViewById(R.id.backbtnlg);
+        back_lgn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent n = new Intent(login_page.this,MainActivity.class);
+                startActivity(n);
+            }
+        });
     }
 
 
--- app/src/main/java/com/example/myapplication/signup_page.java
+++ app/src/main/java/com/example/myapplication/signup_page.java
@@ -1,13 +1,79 @@
 package com.example.myapplication;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class signup_page extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.signup);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        Button sign_up_btn = findViewById(R.id.signup);
+
+        EditText lg_box = findViewById(R.id.email);
+
+        EditText pass_box = findViewById(R.id.password);
+
+        EditText confirm_pass = findViewById(R.id.confirm_password);
+        sign_up_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String email = lg_box.getText().toString();
+                String password = pass_box.getText().toString();
+                String confirm_password = confirm_pass.getText().toString();
+
+
+                if(password.equals(confirm_password)) {
+                    mAuth.createUserWithEmailAndPassword(email, password)
+                            .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                                @Override
+                                public void onComplete(@NonNull Task<AuthResult> task) {
+                                    if (task.isSuccessful()) {
+                                        // Sign in success, update UI with the signed-in user's information
+                                        Toast.makeText(signup_page.this, "Authentication successful.",
+                                                Toast.LENGTH_SHORT).show();                                    } else {
+                                        // If sign in fails, display a message to the user.
+                                        Toast.makeText(signup_page.this, "Authentication failed.",
+                                                Toast.LENGTH_SHORT).show();
+                                    }
+                                }
+                            });
+                }
+                else{
+                    Toast.makeText(signup_page.this,"Passwords do not match",Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+
+
+    }
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if(currentUser != null){
+            currentUser.reload();
+        }
     }
 
 
