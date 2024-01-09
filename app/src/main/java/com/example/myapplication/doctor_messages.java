09a8fe41bfac6501ae0d88b23f14ccee7c92a5a2
yarin levi <yarinlevi139@gmail.com>
2024-01-09 12:24:56 +0200

    updates

---
+++ app/src/main/java/com/example/myapplication/doctor_messages.java
@@ -0,0 +1,12 @@
+package com.example.myapplication;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class doctor_messages extends AppCompatActivity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.doc_messages);
+    }
+}
